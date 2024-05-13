275e5e4fb3e7c238ce4c3b9b00bad8d8a5ec2308
JuhahuJ <lehtinen05@gmail.com>
2024-05-14 00:57:28 +0300

    done with 2.6-2.10

---
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+
+const Filter = ({value, onChange}) => {
+  return (
+    <div>filter shown with: <input value={value} onChange={onChange}/></div>
+  )
+}
+
+const PersonForm = ({newName, newNumber, handleNewName, handleNewNumber, addPerson}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>name: <input value={newName} onChange={handleNewName}/></div>
+      <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
+}
+
+const Person = ({name, number}) => <p>{name} {number}</p>
+
+const Persons = ({persons, search}) => persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())).map(person => <Person key={person.name} name={person.name} number={person.number}/>)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook.`)
+      setNewName("")
+      setNewNumber("")
+    } else {
+    setPersons(persons.concat({name: newName, number: newNumber}))
+    setNewName("")
+    setNewNumber("")
+    }
+  }
+
+  const handleNewName = (event) => setNewName(event.target.value)
+  const handleNewNumber = (event) => setNewNumber(event.target.value)
+  const handleSearch = (event) => setSearch(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter value={search} onChange={handleSearch}/>
+      <h3>add a new</h3>
+      <PersonForm 
+      newName={newName} 
+      handleNewName={handleNewName} 
+      newNumber={newNumber} 
+      handleNewNumber={handleNewNumber} 
+      addPerson={addPerson}/>
+      <h3>Numbers</h3>
+      <Persons persons={persons} search={search}/>
+    </div>
+  )
+
+}
+
+export default App
