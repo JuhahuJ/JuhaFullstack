c277c291a82325b9719a0642ab6c40a91efe7e21
JuhahuJ <lehtinen05@gmail.com>
2024-05-27 16:42:12 +0300

    done with 2.20

--- Osa2/maiden_tiedot/src/App.jsx
+++ Osa2/maiden_tiedot/src/App.jsx
@@ -8,7 +8,10 @@ const Filter = ({value, onChange}) => {
   )
 }
 
-const Country = ({country}) => {
+const Country = ({country, weather, loading}) => {
+  if (loading) {
+    return <p>Loading weather data...</p>
+  }
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -24,11 +27,15 @@ const Country = ({country}) => {
         ))}
       </ul>
       <img src={country.flags.png}/>
+      <h2>Weather in {country.capital}</h2>
+      <p>temperature {weather.main.temp} Celsius</p>
+      <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+      <p>wind {weather.wind.speed} m/s</p>
     </div>
   )
 }
 
-const Countries = ({countries, search, showCountry}) => {
+const Countries = ({countries, search, showCountry, weather, loading}) => {
   const filtered = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
   if (search === '') {
     return <p>Please search for countries</p>
@@ -37,13 +44,16 @@ const Countries = ({countries, search, showCountry}) => {
   } else if (filtered.length <= 10 && filtered.length > 1) {
     return filtered.map(country => <p key={country.name.common}>{country.name.common}<button onClick={() => showCountry(country)}>show</button></p>)
   } else if (filtered.length == 1) {
-    return <Country country={filtered[0]}/>
+    return <Country country={filtered[0]} weather={weather} loading={loading}/>
   } else return <p>No matches found</p>
 }
 
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [weather, setWeather] = useState([])
   const [search, setSearch] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(null)
+  const [loading, setLoading] = useState(true)
 
 
   useEffect(() => {
@@ -52,18 +62,33 @@ const App = () => {
     .then(countries => {
       setCountries(countries)
     })
-}, [])
+  }, [])
+
+  useEffect(() => {
+    if (selectedCountry) {
+      setLoading(true)
+      const lat = selectedCountry.capitalInfo.latlng[0]
+      const lon = selectedCountry.capitalInfo.latlng[1]
+      weatherService.getPlace(lat, lon)
+        .then(weather => {
+          setWeather(weather)
+          setLoading(false)
+        })
+    }
+  }, [selectedCountry])
 
   const handleSearch = (event) => setSearch(event.target.value)
 
   const showCountry = country => {
     setSearch(country.name.common)
+    setSelectedCountry(country)
   }
+
 
   return (
     <div>
       <Filter value={search} onChange={handleSearch}/>
-      <Countries countries={countries} search={search} showCountry={showCountry}/>
+      <Countries countries={countries} search={search} showCountry={showCountry} weather={weather} loading={loading}/>
     </div>
   )
 
--- Osa2/maiden_tiedot/src/services/weather.js
+++ Osa2/maiden_tiedot/src/services/weather.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const api_key = import.meta.env.VITE_WEATHER_KEY
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
+const getPlace = (lat, lon) => {
+  const request = axios.get(`${baseUrl}lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
   return request.then(response => response.data)
 }
 
-export default { getAll}
+export default { getPlace }
